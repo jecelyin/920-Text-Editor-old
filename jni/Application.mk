dfb79b66cbb464c88d1863858000cc9cb5dde6a0
Brendan Le Foll <brendan.le.foll@intel.com>
2013-10-31 15:16:13 -0100

    Add x86 support

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,5 +1,5 @@
 # The ARMv7 is significanly faster due to the use of the hardware FPU
-APP_ABI := armeabi armeabi-v7a
+APP_ABI := armeabi armeabi-v7a x86
 APP_PLATFORM ：= android-8
 
 APP_BUILD_SCRIPT := $(call my-dir)/highlight/Android.mk \
